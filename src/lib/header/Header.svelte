d4f9755dc7f1a8020441923ec7097008dd25085c
Thor Muller <thor@thormuller.com>
2022-03-10 19:28:23 +0100

    quibbles

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,4 +1,6 @@
-
+<script>
+	export let page
+</script>
 
 <header>
 
@@ -35,20 +37,6 @@
 	.corner {
 		width: 3em;
 		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
 	}
 
 	nav {
